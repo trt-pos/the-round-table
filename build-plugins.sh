cc59434dba66857f86bfef9cfe900a171a3eaf87
Borja Castellano <borjacastellano1@gmail.com>
2025-02-18 08:57:43 +0100

    izpack xml preview

--- build-plugins.sh
+++ build-plugins.sh
@@ -1,40 +1,51 @@
 #!/bin/bash
 
 rm -rf "desktop-app/plugins"
+mkdir -p "desktop-app/plugins"
 
-mkdir "desktop-app/plugins"
-
-cash-register()
-{
-    cd "plugin-cash-register" || exit
+cash-register() {
+    cd "plugin-cash-register" || exit 
     mvn clean package
     cd ..
 }
 
-receipt-manager()
-{
+receipt-manager() {
     cd "plugin-receipt-manager" || exit
+    mvn clean package 
+    cd ..
+}
+
+table-drawing() {
+    cd "plugin-table-drawing" || exit 
+    mvn clean package 
+    cd ..
+}
+
+spanish-billing() {
+    cd "plugin-spanish-billing" || exit 
     mvn clean package
     cd ..
 }
 
-table-drawing()
-{
-    cd "plugin-table-drawing" || exit
-    mvn clean package
-    cd ..
-}
+plugins=(
+    cash-register
+    receipt-manager
+    table-drawing
+    spanish-billing
+)
 
-spanish-billing()
-{
-    cd "plugin-spanish-billing" || exit
-    mvn clean package
-    cd ..
-}
-
-cash-register &
-receipt-manager &
-table-drawing &
-spanish-billing &
-
-wait 
+PLUGIN_NAME=$1
+if [[ -n "$PLUGIN_NAME" ]]; then
+    if [[ ${plugins[@]} =~ "${PLUGIN_NAME}" ]]; then
+        # Ejecutar solo el plugin especificado
+        $PLUGIN_NAME
+    else
+        echo "Error: Plugin '$PLUGIN_NAME' not found"
+        exit 1
+    fi
+else
+    for plugin in "${plugins[@]}"; do
+        $plugin &
+    done
+    wait
+fi
--- install-plugins.sh
+++ install-plugins.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-bash build-plugins.sh
+PLUGIN_NAME=$1
+
+bash build-plugins.sh "$PLUGIN_NAME"
 
 mkdir "$HOME/.round-table-dev/plugins"
 cp -r desktop-app/plugins/* ~/.round-table-dev/plugins/
